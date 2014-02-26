70e06c5c462ed7812b69744efe2520ed4a317c8a
safonseca <santiagofonseca04@gmail.com>
2014-02-25 17:01:52 -0800

    Random Motion("Vibration" and Flipping)

--- clicks/clicks.js
+++ clicks/clicks.js
@@ -1,8 +1,17 @@
 /*global $*/
 /*jslint sloppy:true, browser: true*/
-$('h1.page-title').text('It worked!');
+$('h1.page-title').text('Such Amaze!');
 $(window).on('click', function (e) {
     $('img.logo').css({left: e.clientX,
                        top: e.clientY});
-    $('img.logo').toggleClass('hide');
+    $('img.logo').toggleClass('rotated');
 });
+function randomMargin() {
+    return Math.random() * 200 - 100;
+}
+setInterval(function () {
+    $('.logo').css({
+        'margin-left': randomMargin(),
+        'margin-top': randomMargin()
+    });
+}, 200);

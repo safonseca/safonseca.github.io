7c9e79daeffe42596db8badc9f9972c3b684b139
safonseca <santiagofonseca04@gmail.com>
2014-02-25 17:55:52 -0800

    Circular Motion/Movement of the Logo

--- clicks/clicks.js
+++ clicks/clicks.js
@@ -1,5 +1,6 @@
 /*global $*/
 /*jslint sloppy:true, browser: true*/
+/*
 $('h1.page-title').text('Such Amaze!');
 $(window).on('click', function (e) {
     $('img.logo').css({left: e.clientX,
@@ -8,10 +9,36 @@ $(window).on('click', function (e) {
 });
 function randomMargin() {
     return Math.random() * 200 - 100;
-}
+} 
+*/
+var vx = Math.random() * 10 - 5;
+var vy = Math.random() * 10 - 5;
+var x = $(window).width() / 2;
+var y = $(window).height() / 2;
+var theta = 0;
+var vtheta = Math.PI / 180;
+var r = 60;
 setInterval(function () {
     $('.logo').css({
-        'margin-left': randomMargin(),
-        'margin-top': randomMargin()
+        'left': x + r * Math.cos(theta),
+        'top': y + r * Math.sin(theta)
     });
-}, 200);
+    theta = theta + vtheta;
+   /*
+    x = x + vx;
+    y = y + vy;
+   if (x < 0) {
+        x = 0;
+        vx = -vx;
+    } else if (x > $(window).width()) {
+        x = $(window).width();
+        vx = -vx;
+    }
+    if (y < 0) {
+        y = 0;
+        vy = -vy;
+    } else if (y > $(window).height()) {
+        y = $(window).height();
+        vy = -vy;
+    }*/
+}, 20);
